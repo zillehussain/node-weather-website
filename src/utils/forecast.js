b78c51a07dcf196f54def21dde886a2adac23d84
zillehussain <87498070+zillehussain@users.noreply.github.com>
2022-10-19 23:07:07 +0530

    temperature unit change
    changed unit to celsius

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=24e6cad4666b123cc14d2a7e71345f2e&query=' + latitude + ',' + longitude + '&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=24e6cad4666b123cc14d2a7e71345f2e&query=' + latitude + ',' + longitude + '&units=m'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
